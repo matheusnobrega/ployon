fe2018fb6e75cc516e7b4736c822dd921d8258fa
Matheus <matheus.nobrega.duarte@gmail.com>
2022-10-11 20:32:22 -0300

    Adicionado mensagens e tópicos

--- base/views.py
+++ base/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms = [
-    {'id':1, 'name':'Let\'s learn greek!'},
-    {'id':2, 'name':'Enuma elish in the original'},
-    {'id':3, 'name':'Aramaic for beginners!'},
-]
+# rooms = [
+#     {'id':1, 'name':'Let\'s learn greek!'},
+#     {'id':2, 'name':'Enuma elish in the original'},
+#     {'id':3, 'name':'Aramaic for beginners!'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     return render(request, 'base/home.html', {'rooms':rooms})
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     return render(request, 'base/room.html', {'room':room})
